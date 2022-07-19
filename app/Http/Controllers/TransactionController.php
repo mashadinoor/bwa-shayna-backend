6773602916f644ebea0055f2997877f52bfcc231
mashadinoor <mashadinoor@gmail.com>
2022-07-19 14:30:42 +0700

    Transaction edit status, and delete transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -84,7 +84,11 @@ class TransactionController extends Controller
    */
   public function edit($id)
   {
-    //
+    $item = Transaction::findOrFail($id);
+
+    return view('pages.transactions.edit')->with([
+      'item' => $item
+    ]);
   }
 
   /**
@@ -96,7 +100,12 @@ class TransactionController extends Controller
    */
   public function update(Request $request, $id)
   {
-    //
+    $data = $request->all();
+
+    $item = Transaction::findOrFail($id);
+    $item->update($data);
+
+    return redirect()->route('transactions.index');
   }
 
   /**
@@ -107,6 +116,23 @@ class TransactionController extends Controller
    */
   public function destroy($id)
   {
-    //
+    $item = Transaction::findOrFail($id);
+    $item->delete();
+
+    return redirect()->route('transaction.index');
+  }
+
+  public function setStatus(Request $request, $id)
+  {
+    $request->validate([
+      'status' => 'required|in:PENDING,SUCCESS,FAILED'
+    ]);
+
+    $item = Transaction::findOrFail($id);
+    $item->transaction_status = $request->status;
+
+    $item->save();
+
+    return redirect()->route('transaction.index');
   }
 }
--- resources/views/pages/transactions/index.blade.php
+++ resources/views/pages/transactions/index.blade.php
@@ -45,18 +45,18 @@
                     </td>
                     <td>
                       @if($item->transaction_status == 'PENDING')
-                        {{-- <a href="{{ route('transactions.status', $item->id) }} ? status=SUCCESS" class="btn btn-success btn-sm">
+                        <a href="{{ route('transactions.status', $item->id) }}?status=SUCCESS" class="btn btn-success btn-sm">
                           <i class="fa fa-check"></i>
                         </a>
-                        <a href="{{ route('transactions.status', $item->id) }} ? status=FAILED" class="btn btn-warning btn-sm">
+                        <a href="{{ route('transactions.status', $item->id) }}?status=FAILED" class="btn btn-warning btn-sm">
                           <i class="fa fa-times"></i>
-                        </a> --}}
+                        </a>
                       @endif
                       <a href="#mymodal"
                         data-remote="{{ route('transactions.show', $item->id) }}"
                         data-toggle="modal"
                         data-target="#mymodal"
-                        data-title="Detail Transaksi {{ $item->uuid }}"
+                        data-title="Detail Transaksi {{ $item->uuid }}""
                         class="btn btn-info btn-sm">
                         <i class="fa fa-eye"></i>
                       </a>

f9203d02867c2ce2c1ac358185fa7c7e4b72cc7b
mashadinoor <mashadinoor@gmail.com>
2022-07-19 17:00:30 +0700

    API

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+  public function all(Request $request)
+  {
+    $id = $request->input('id');
+    $limit = $request->input('limit', 6);
+    $name = $request->input('name');
+    $slug = $request->input('slug');
+    $type = $request->input('type');
+    $price_from = $request->input('price_from');
+    $price_to = $request->input('price_to');
+
+    if ($id) {
+      $product = Product::with('galleries')->find($id);
+      if ($product)
+        return ResponseFormatter::success($product, 'Data produk berhasil diambil');
+      else
+        return ResponseFormatter::error(null, 'Data produk tidak ada', 404);
+    }
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('products', 'API\ProductController@all');
+
+Route::get('/products', [\App\Http\Controllers\API\ProductController::class, 'all']);
